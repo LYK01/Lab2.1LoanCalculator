2b9370b4fa796e441ed7044ec2746deb67b2b5c6
taruc <leongyk-wa15@student.tarc.edu.my>
2017-11-02 11:39:35 +0800

    Initial

---
+++ app/src/main/java/com/example/taruc/lab21loancalculator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.taruc.lab21loancalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String MONTHLY_PAYMENT = "payment";
+    public static final String LOAN_STATUS = "status";
+
+    private EditText editTextPrice, editTextDownpayment, editTextInterest ,editTextRepayment, editTextSalary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        editTextDownpayment = (EditText) findViewById(R.id.editTextDownpayment);
+
+    }
+
+    public void calculateLoan(View view){
+        //TODO: calculate monthly payment and determine the loan status
+        double monthlyPayment = 0;
+        String status = "Approved";
+        monthlyPayment=((Double.parseDouble(editTextPrice.getText().toString())-Double.parseDouble(editTextDownpayment.getText().toString()))
+                +((Double.parseDouble(editTextPrice.getText().toString())-Double.parseDouble(editTextDownpayment.getText().toString()))
+                *Double.parseDouble(editTextInterest.getText().toString())*(Double.parseDouble(editTextRepayment.getText().toString())/12)))/
+                (Double.parseDouble(editTextRepayment.getText().toString()));
+
+        if(Double.parseDouble(editTextSalary.getText().toString()) > Double.parseDouble(editTextSalary.getText().toString())* 0.3)
+            status = "Rejected";
+
+        //Create an Explicit Intent
+        Intent intent = new Intent(this, ResultActivity.class);
+        //TODO: passing data using putExtra method
+        //format: putExtra(TAG, value)
+        intent.putExtra(MONTHLY_PAYMENT, monthlyPayment);
+        intent.putExtra(LOAN_STATUS, status);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/taruc/lab21loancalculator/ResultActivity.java
@@ -0,0 +1,36 @@
+package com.example.taruc.lab21loancalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+
+    private TextView textViewMonthlyPayment, textViewStatus;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        textViewMonthlyPayment = (TextView) findViewById(R.id.textViewMonthlyPayment);
+        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+
+        //To receive data from another Activity
+        Intent intent = getIntent(); //Asking "who called me?"
+        double payment = intent.getDoubleExtra(MainActivity.MONTHLY_PAYMENT, 0);
+        String status = intent.getStringExtra(MainActivity.LOAN_STATUS);
+        //TODO: Display the outputs
+
+
+        textViewMonthlyPayment.setText("Monthly Payment " + payment);
+        textViewStatus.setText("Status " + status);
+    }
+
+    public void closeActivity(View view){
+        //Terminate the current activity
+        finish();
+    }
+}
